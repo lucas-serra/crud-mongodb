b9491fb2f633c1b3c76237c294b05ae046ff1873
lucas-serra <lucasdserra1234@gmail.com>
2023-02-15 16:26:33 -0300

    CRUD spring boot e mongodb

---
+++ src/main/java/com/agro/arroba/service/UsuarioService.java
@@ -0,0 +1,14 @@
+package com.agro.arroba.service;
+
+import java.util.List;
+
+import com.agro.arroba.model.Usuario;
+
+public interface UsuarioService {
+
+	public List<Usuario> obterTodos();
+	
+	public Usuario obterPorCodigo(String codigo);
+	
+	public Usuario criar(Usuario usuario);
+}
